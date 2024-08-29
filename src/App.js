6ff8eeae25c28c624d2393290fe7af28de886f27
JZD3N <jacobquarshie01@gmail.com>
2024-08-29 14:10:46 -0700

    Changed dashboard

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignUp from './pages/sign-up';
 import DashBoard from './pages/dashboard';
 import SignIn from './pages/sign-In';
+import NavBar from './components/navbar';
 
 
 function App() {
   return (
     <Router>
+      <NavBar/>
       <Routes>
         {/* Define the URL paths for each page */}
         <Route path="/" element={<SignIn />} />
