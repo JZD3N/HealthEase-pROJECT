54cd8dbb2243b4cd13436c8367ff60176e557aba
JZD3N <jacobquarshie01@gmail.com>
2024-09-14 01:05:25 -1200

    Added API Keys and updates

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./src/config/database');
 const routes = require('./src/routes/routes');
-const mapAPI = require('./src/controllers/mapAPI')
+const mapAPI = require('./src/controllers/maps')
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 // Connect to MongoDB
 connectDB()
@@ -16,6 +16,7 @@ app.use(cors());
 
 // Define your API routes here
 app.use('./src/routes',routes)
+app.use('/api/maps',mapAPI)
 
 app.listen(port, () =>{
   console.log(`Server listening on port ${port}`);
